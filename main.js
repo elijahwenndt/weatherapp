2dd838fae0d98aa21226144e1fe9694b41f96065
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-09-29 15:06:06 +0000

    fixed structure problems, beginning dynamic generation

--- main.js
+++ main.js
@@ -9,13 +9,11 @@ let stateUpdateData = {
     image: ""
 }
 console.log(stateUpdateData)
-// const grabForm = document.getElementById('submission')
-// const grabText = document.getElementById('zipCode')
+
 const grabForm = document.getElementById('submission')
  grabForm.addEventListener('submit', e => {
      e.preventDefault()
     
-    //const grabForm = document.getElementById('submission')
     const grabText = document.getElementById('zipCode')
     console.log(grabText.value)
     let newZip = grabText.value
@@ -53,25 +51,7 @@ const grabForm = document.getElementById('submission')
 
 
 
-// let findSub = document.getElementById('submission')
 
-// findSub.addEventListener('submit', buttonClick)
-
-// async function getData(url) {
-//     try {
-//         let response = await axios.get(url)
-//         console.log(response.data)
-//         document.getElementById('fetchTarget1').innerHTML= response.data.name
-//         document.getElementById('fetchTarget2').innerHTML= response.data.main.temp
-//         document.getElementById('fetchTarget3').innerHTML= response.data.weather[0].main
-//         document.getElementById('fetchTarget4').innerHTML= response.data.weather[0].icon
-//     } 
-//     catch (error) {
-//         console.error(error)
-//     }
-
-// }
-// getData('https://api.openweathermap.org/data/2.5/weather?zip=40514,us&appid=3f7b74a1bdfcaad595048157e8c38426')
 
 
 
