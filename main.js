308365f9272300713b2a71a9951fb1f2a55c3347
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-09-30 12:53:18 +0000

    ok commented up my code and set another image, im shipping it

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 //thank you to instructors(johnathon and josh), dakota, josh dale, estus for helping me get on the right path
+
+//function that will take in parameters. based on parameters defined later on it grabs what the parent element will be
+//what the element type it will create, what to set the id too, and what to add to the classes
+//EX-if its on the parameter setClass, it will set the attribute (defined class) to the parameter passed into setClass
+//same for setId. at the end it takes the parameter in the first position and defines that as the parent to append too
 function setStructure(parent, elementType, setId, setClass) {
     let element = document.createElement(elementType)
     if (setClass) {
@@ -9,9 +14,12 @@ function setStructure(parent, elementType, setId, setClass) {
     }
     parent.appendChild(element)
 }
+
+//set up the initial id/classes on the container
 const container = document.getElementById('container')
 container.setAttribute("class", "d-grid gap-4 text-center")
 
+//begin calling the function, and passes in the parameters. obviously this isnt the most elegant solution as i call the function 20 times. will refactor this weekend
 setStructure(container, 'div', 'row1', 'row mt-3')
 setStructure(row1, 'div', 'weather', 'col-12')
 setStructure(row1, 'div', 'instructionText', 'col-12')
@@ -35,7 +43,7 @@ setStructure(container, 'div', 'row5', 'row')
 setStructure(row5, 'div', 'conditionText', 'col-12')
 setStructure(row5, 'div', 'fetchTarget5', 'col-12')
 
-
+//sets up a blank global object to catch the info coming out of axios fetch
 let stateUpdateData = {
     city: "",
     temp: {
@@ -48,21 +56,26 @@ let stateUpdateData = {
 }
 console.log(stateUpdateData)
 
+//sets up event listener
 const grabForm = document.getElementById('submission')
  grabForm.addEventListener('submit', e => {
+
+    //again not the most elegant solution, entire call happens in the submit eventlistener function
      e.preventDefault()
     
+     //next i grab the zipcode then set the value = a new variable
     const grabText = document.getElementById('zipCode')
     console.log(grabText.value)
     let newZip = grabText.value
     console.log(newZip.length)
 
+    //set up a statement to catch if the zipcode is invalid
     if (newZip.length === 5) {
     async function getData(url) {
         try {
             let response = await axios.get(url)
             console.log(response)
-            //call to function, inside parathensis passes data to called function
+            //call to passState function, inside parathensis passes data to called function
             passState(response.data)
             
         } 
@@ -79,6 +92,8 @@ const grabForm = document.getElementById('submission')
     }
  })
 
+    //this function gets the API info from above then fills the global variables 
+    //after that i set up conditionals to add css styling that sets the background image based on data coming from the data.weather[0].main
     function passState(data) {
         
         stateUpdateData.city= data.name
@@ -89,6 +104,8 @@ const grabForm = document.getElementById('submission')
         let setBG = data.weather[0].main
         console.log(setBG)
 
+        //sorry yall, had to do this the cheap way. creates an element style then sets that style to the string that is provided
+        //after that i append the style to the HEAD that way it doesnt mess with any of my dynamic page generation
         if (setBG == 'Clear'){
             let style = document.createElement('style');
             style.innerHTML = `
@@ -161,9 +178,21 @@ const grabForm = document.getElementById('submission')
             `;
             document.head.appendChild(style);
         }
+        else if (setBG =='Fog'){
+            let style = document.createElement('style');
+            style.innerHTML = `
+            #bg-set {
+                background: url("fog.jpg") no-repeat center center fixed;
+                background-size: cover;
+            }
+            `;
+            document.head.appendChild(style);
+        }
         
         putOnPage()
     }
+    
+    //where the magic happens, fills the page with the proper data that has now been grabbed from the global object.
     function putOnPage (fill) {
         document.getElementById('weather').innerHTML = 'Tranquil Weather'
         document.getElementById('instructionText').innerHTML = 'Type in a zip code and hit enter'
